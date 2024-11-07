c759fb7a94d8d9ca1c8f8542f389dda1347bf320
NPT0116 <thanh1612004@gmail.com>
2024-11-07 22:12:49 +0700

    adding pagination and response type

--- obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
+++ obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4fc8cbcd4d125c3044509a970b7d490ebbad88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb48341377d2de8f3f0c069252f2718668c84d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
