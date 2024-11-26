03cddf6ea0779e0db845c112236a8783110b9762
devsirose <dev.sirose@gmail.com>
2024-11-27 01:10:15 +0700

    update reservation flow

--- obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
+++ obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3baf7c0a9531453f68dbfd5f5920398db57d4239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d964ce0114cbb08a205ad38cd0ab05100d21dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
