59f1b299493bdd19b9852b07fd2d1004ecf6599f
NPT0116 <thanh1612004@gmail.com>
2024-11-26 11:38:40 +0700

    merge main

--- obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
+++ obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c7edb7289fd11a8a7e8fcc503d3dc7ea902c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0101b409fec01faeaa589b0d1a830762ed278050")]
 [assembly: System.Reflection.AssemblyProductAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
