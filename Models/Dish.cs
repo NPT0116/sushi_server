76d4927f9b033a5194c6f69be4b2096123aab05b
devsirose <126336138+devsirose@users.noreply.github.com>
2024-11-28 22:15:21 +0700

    merge branch dev into main (#3)
    * update submit order
    
    * update seed reservation
    
    * update order response
    
    * update order detail response

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Dto.Dish;
 
 namespace sushi_server.Models;
 
@@ -15,6 +16,13 @@ public class Dish
     public Guid SectionId { get; set; }
     [ForeignKey("SectionId")]
     public Section Section { get; set; } = null!;
+    public List<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
 
-
+    public DishResponseDTO toResponseDTO() {
+        return new DishResponseDTO {
+            DishId = this.DishId,
+            DishName = this.DishName,
+            CurrentPrice = this.CurrentPrice
+        };
+    }
 }
