c41706de823e0cdaefdc222c277014b884902f8a
NPT0116 <thanh1612004@gmail.com>
2024-12-13 21:15:17 +0700

    nothing

--- obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
+++ obj/Debug/net8.0/sushi_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc10b158500d0281fad69cc42a72c54f58aeb579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386d5f87d60fc404b2d69902e758697d99551029")]
 [assembly: System.Reflection.AssemblyProductAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sushi_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
